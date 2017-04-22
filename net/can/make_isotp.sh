314ea73e97934a1f0f0d13404e9ec4f979fb1b35
Oliver Hartkopp <socketcan@hartkopp.net>
2017-04-22 13:01:35 +0200

    CAN ISO-TP has moved to https://github.com/hartkopp/can-isotp.git
    Due to this fix from Eric Dumazet
    https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit?id=f1712c73714088a7252d276a57126d56c7d37e64
    the can_rx_register() function parameters have been changed which finally
    renders the current approach to build the can-isotp modules unmaintainable.
    
    Thankfully Nathan L. Conrad created a simplified build setup for can-isotp at
    https://github.com/downwith/linux-isotp and it was ok for him to use his work.
    
    The latest can-isotp source is now at https://github.com/hartkopp/can-isotp.git
    
    Signed-off-by: Oliver Hartkopp <socketcan@hartkopp.net>

--- net/can/make_isotp.sh
+++ net/can/make_isotp.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+echo "CAN ISO-TP has moved to https://github.com/hartkopp/can-isotp.git"
+echo "This repository is not maintained anymore."
+echo "Are you sure you want to continue the build? (if so press ENTER)"
+read NONONEVER
 make CONFIG_CAN_ISOTP=m CONFIG_CAN=n CONFIG_CAN_RAW=n CONFIG_CAN_BCM=n CONFIG_CAN_GW=n $@
